13e95bee23e5353a725aa3260ec65bc2cfd95cb0
Kevin Miller <kevin@neomantra.net>
2023-08-14 11:17:38 -0500

    moar tests
    Signed-off-by: Kevin Miller <kevin@neomantra.net>

--- ymdflag_test.go
+++ ymdflag_test.go
@@ -66,3 +66,49 @@ func TestValidateYMD(t *testing.T) {
 	err = ValidateYMD(-1)
 	assert.Error(t, err, "negative date")
 }
+
+func TestAsYearMonthDay(t *testing.T) {
+
+	// default is zero
+	flag := YMDFlag{}
+	year, month, day := flag.AsYearMonthDay()
+	assert.Equal(t, 0, year)
+	assert.Equal(t, 0, month)
+	assert.Equal(t, 0, day)
+
+	flag = NewYMDFlag(time.Date(2022, 1, 2, 3, 4, 5, 6, time.UTC))
+	year, month, day = flag.AsYearMonthDay()
+	assert.Equal(t, 2022, year)
+	assert.Equal(t, 1, month)
+	assert.Equal(t, 2, day)
+}
+
+func TestNewFlagFromInt(t *testing.T) {
+	flag, err := NewYMDFlagFromInt(0)
+	assert.NoError(t, err, "zero is ok")
+
+	year, month, day := flag.AsYearMonthDay()
+	assert.Equal(t, 0, year)
+	assert.Equal(t, 0, month)
+	assert.Equal(t, 0, day)
+}
+
+func TestStringToYMD(t *testing.T) {
+
+	yyyymmdd, err := StringToYMD("20220101")
+	assert.NoError(t, err, "valid date should not return an error")
+	assert.Equal(t, 20220101, yyyymmdd)
+
+	_, err = StringToYMD("2022-01-01")
+	assert.Error(t, err, "date string should not have separators")
+
+	_, err = StringToYMD("hello world")
+	assert.Error(t, err, "non numeric string should return an error")
+
+	_, err = StringToYMD("123456789")
+	assert.Error(t, err, "too long string")
+
+	yyyymmdd, err = StringToYMD("")
+	assert.NoError(t, err, "empty string should not return an error")
+	assert.Equal(t, 0, yyyymmdd)
+}
